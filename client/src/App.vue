17021d86a5ae896cda7d9972ff1dedd73a2966ce
Deven Pritchard <pritchad1@newpaltz.edu>
2026-03-05 15:15:19 -0500

    Attempting to add NavBar to Client

---
+++ client/src/App.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar.vue';
+</script>
+
+<template/>
+  <NavBar />
+
+  <RouterView />
