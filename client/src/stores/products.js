8d0409882444d6896c66f34f37d4f230f07d3cdd
Deven Pritchard <pritchad1@newpaltz.edu>
2026-03-14 11:48:38 -0400

    Did the work in the wrong project all these files were carried over from what we did in class. However I was working in the at home folder

--- client/src/stores/products.js
+++
@@ -1,8 +0,0 @@
-import { defineStore } from 'pinia';
-import { ref } from 'vue';
-export const useProductStore = defineStore('product', () => {
-    const products = ref([]);
-    return {
-        products,
-    };
-});
